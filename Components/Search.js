cf53c95c203171dcb9dce1482498092cd97ced51
Elian Arnaud <arnaud35u@etu.univ-lorraine.fr>
2022-03-21 15:44:45 +0100

    Modification du style du TextInput et de la View

--- Components/Search.js
+++ Components/Search.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import { View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button } from 'react-native'
+
+
+const styles = StyleSheet.create({
+    textinput: {
+        margin: 'auto',
+        fontSize: 15,
+        textAlign: "center",
+        height: 40,
+        borderWidth: 1,
+        paddingLeft: 5,
+    },
+    view: {
+        marginTop: 40,
+        backgroundColor:"#212121"
+    }
+  
+})
 
 class Search extends React.Component {
     render() {
         return (
-            <View style={{marginTop: 50}}>
-                <TextInput placeholder='Titre du film'/>
-                <Button title='Rechercher' onPress={() => {}}/>
+            <View style={styles.view}>
+                <TextInput style={styles.textinput} placeholderTextColor='white' placeholder='Titre du film'/>
+                <Button title='Rechercher' onPress={() => {}} />
             </View>
         )
     }
